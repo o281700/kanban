23828df8b03f460f5e8aa2bbd4cdcd7850e84f01
o281700 <siba0311@gmail.com>
2020-12-08 17:13:54 +0900

    NameColumnAdded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_form_forgery with: :exception
+
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_contoroller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+  end
+
 end
