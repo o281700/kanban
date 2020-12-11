0543b16d7a3672597efb39525b6b07d07c728a56
o281700 <siba0311@gmail.com>
2020-12-12 04:47:52 +0900

    pry-rails Introdaction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_154211) do
+ActiveRecord::Schema.define(version: 2020_12_11_194114) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
